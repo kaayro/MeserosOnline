b8835b958efd3c9b405b6d561a55886baa8cdf30
Carlos Celma <solrac@igitsoft.com>
2013-08-15 10:08:03 -0500

    a

--- js/ajax-conn.js
+++ js/ajax-conn.js
@@ -73,18 +73,20 @@ var serverFile='http://192.168.1.70/carlos/APPS/mitierraoaxaca/Web/fnc/ajaxfnc2.
 //Abrir Mesas
     function openTable(id){
         //Crear Nueva Orden
-        $.ajax({
-            type: 'POST',
-            url: serverFile,
-            data: 'fnc=openTable&id='+id,
-            error: function(xhr, type){
-                alert('Ajax error!');
-            }
-        }).done(function(orden){
-            if(orden>0){
-                $('#tables').append('<li id="tab'+id+'" orden="'+orden+'">Mesa '+id+'</li>');
-            }
-        });
+        navigator.notification.prompt('A nombre de:',function(results){
+            $.ajax({
+                type: 'POST',
+                url: serverFile,
+                data: 'fnc=openTable&id='+id+'&nom='+results.input1,
+                error: function(xhr, type){
+                    alert('Ajax error!');
+                }
+            }).done(function(orden){
+                if(orden>0){
+                    $('#tables').append('<li id="tab'+id+'" orden="'+orden+'">Mesa '+id+'</li>');
+                }
+            });
+        },'Orden',['Aceptar','Cancelar'],'Anónimo');
     }
 //Obtener Pedidos de la Orden
     function getPedidosOrden(oId){
