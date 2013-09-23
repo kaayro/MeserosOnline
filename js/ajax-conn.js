15150d6ba70c6ff06401fb4679ced6a48577488e
Carlos Celma <solrac@igitsoft.com>
2013-09-23 02:02:07 -0500

    c

--- js/ajax-conn.js
+++ js/ajax-conn.js
@@ -56,6 +56,8 @@ var serverFile='http://192.168.1.69/carlos/APPS/mitierraoaxaca/Web/fnc/ajaxfnc2.
                     clase = 'class="opt"';
                     if(tables[i].stat==0)
                         clase = 'class="pending"';
+					if(tables[i].stat==1)
+						clase = 'class="alert"';
                     $('#tables').append('<li id="tab'+tables[i].mesaId+'" '+clase+' orden="'+tables[i].ordenId+'">Mesa '+tables[i].mesaId+'</li>');
                 }
             }
